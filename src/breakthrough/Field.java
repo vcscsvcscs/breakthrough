1ccb01a5c72d5267651f6a4c293bcfa439ec1359
Vargha Csongor <Culisa1023@gmail.com>
2023-11-16 16:16:06 +0000

    Field + LazyPawn implementation

--- src/breakthrough/Field.java
+++ src/breakthrough/Field.java
@@ -1,42 +1,74 @@
 package breakthrough;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.awt.Color;
 
 /**
+ * The {@code Field} class represents a single field on the game board in the Breakthrough game.
+ * Each field can have a color and may contain a pawn.
  *
- * @author v5inla 
+ * @author v5inla
  */
 public class Field {
 
+    /**
+     * The color of the field.
+     */
     private Color color;
-    private int number;
 
+    /**
+     * The pawn present on the field.
+     */
+    private LazyPawn pawn;
+
+    /**
+     * Constructs a new {@code Field} with no specified color and no pawn.
+     */
     public Field() {
         color = null;
-        number = -1;
+        pawn = null;
     }
 
+    /**
+     * Constructs a new {@code Field} with specified color and pawn.
+     */
+    public Field(Color color,LazyPawn pawn) {
+        this.color = color;
+        this.pawn = pawn;
+    }
+
+    /**
+     * Gets the color of the field.
+     *
+     * @return The color of the field.
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Sets the color of the field.
+     *
+     * @param color The color to set for the field.
+     */
     public void setColor(Color color) {
         this.color = color;
     }
 
-    public int getNumber() {
-        return number;
+    /**
+     * Gets the pawn present on the field.
+     *
+     * @return The pawn on the field.
+     */
+    public LazyPawn getPawn() {
+        return pawn;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    /**
+     * Sets the pawn on the field.
+     *
+     * @param pawn The pawn to be placed on the field.
+     */
+    public void setPawn(LazyPawn pawn) {
+        this.pawn = pawn;
     }
-
 }
--- src/breakthrough/LazyPawn.java
+++ src/breakthrough/LazyPawn.java
@@ -1,30 +1,43 @@
 package breakthrough;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.awt.Color;
 
 /**
+ * The {@code LazyPawn} class represents a pawn in the Breakthrough game with a specified color.
+ * Pawns are game pieces that can be placed on the game board fields.
  *
  * @author v5inla
  */
 public class LazyPawn {
 
+    /**
+     * The color of the pawn.
+     */
     private Color color;
 
-    public LazyPawn() {
-        color = null;
+    /**
+     * Constructs a new {@code LazyPawn} with the specified color.
+     *
+     * @param color The color of the pawn.
+     */
+    public LazyPawn(Color color) {
+        this.color = color;
     }
 
+    /**
+     * Gets the color of the pawn.
+     *
+     * @return The color of the pawn.
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Sets the color of the pawn.
+     *
+     * @param color The color to set for the pawn.
+     */
     public void setColor(Color color) {
         this.color = color;
     }
