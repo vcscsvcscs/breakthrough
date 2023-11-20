aa85f78f555fa46c6049853b85331378743cd134
Vargha Csongor <Culisa1023@gmail.com>
2023-11-20 23:32:06 +0100

    Init board gui

--- src/breakthrough/BoardGUI.java
+++ src/breakthrough/BoardGUI.java
@@ -1,131 +1,60 @@
 package breakthrough;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-/**
- *
- * @author v5inla 
- */
 public class BoardGUI {
-
-    private JButton[][] buttons;
     private Board board;
     private JPanel boardPanel;
-    private JLabel timeLabel;
-    private ArrayList<Point> points;
 
-    private Random random = new Random();
-    private int clickNum = 0;
-    private long startTime;
-    private Timer timer;
-
-    private final int NUM_COLORED_FIELDS = 4;
+    private Point sourceCoordinates; // To store the source coordinates of the drag
 
     public BoardGUI(int boardSize) {
+        boardPanel = new JPanel(new GridLayout(boardSize, boardSize));
         board = new Board(boardSize);
-        boardPanel = new JPanel();
-        points = new ArrayList<>();
-        boardPanel.setLayout(new GridLayout(board.getBoardSize(), board.getBoardSize()));
-        buttons = new JButton[board.getBoardSize()][board.getBoardSize()];
-        for (int i = 0; i < board.getBoardSize(); ++i) {
-            for (int j = 0; j < board.getBoardSize(); ++j) {
-                JButton button = new JButton();
-                button.addActionListener(new ButtonListener(i, j));
-                button.setPreferredSize(new Dimension(80, 40));
-                buttons[i][j] = button;
-                boardPanel.add(button);
-                points.add(new Point(i, j));
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < board.getBoardSize(); i++) {
+            for (int j = 0; j < board.getBoardSize(); j++) {
+                FieldPanel fieldPanel = new FieldPanel(new Point(i, j));
+                fieldPanel.addMouseListener(new PieceClickListener());
+                boardPanel.add(fieldPanel);
             }
         }
-        Collections.shuffle(points);
+    }
 
-        timeLabel = new JLabel(" ");
-        timeLabel.setHorizontalAlignment(JLabel.RIGHT);
-        timer = new Timer(10, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                timeLabel.setText(elapsedTime() + " ms");
+    private class FieldPanel extends JPanel {
+        private Point coordinates;
+
+        public FieldPanel(Point coordinates) {
+            this.coordinates = coordinates;
+            setBackground(board.get(coordinates.x, coordinates.y).getColor());
+        }
+    }
+
+    private class PieceClickListener extends MouseAdapter {
+        @Override
+        public void mousePressed(MouseEvent e) {
+            sourceCoordinates = ((FieldPanel) e.getSource()).coordinates;
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            Point destinationCoordinates = ((FieldPanel) e.getSource()).coordinates;
+            if (sourceCoordinates != null && destinationCoordinates != null) {
+                board.movePiece(sourceCoordinates, destinationCoordinates);
+                // Update the GUI and check for game over, etc.
+                // You might need to add code here to update the GUI components based on the game state.
             }
-        });
-        startTime = System.currentTimeMillis();
-        timer.start();
-    }
-
-    public long elapsedTime() {
-        return System.currentTimeMillis() - startTime;
-    }
-
-    public void refresh(int x, int y) {
-        JButton button = buttons[x][y];
-        Field field = board.get(x, y);
-        button.setBackground(field.getColor());
-        if (field.getColor() != null) {
-            button.setText(String.valueOf(field.getNumber()));
-        } else {
-            button.setText("");
+            sourceCoordinates = null; // Reset the source coordinates after the move
         }
     }
 
     public JPanel getBoardPanel() {
         return boardPanel;
     }
-
-    class ButtonListener implements ActionListener {
-
-        private int x, y;
-
-        public ButtonListener(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (board.get(x, y).getColor() == null) {
-                Color color = new Color(random.nextInt(256),
-                        random.nextInt(256), random.nextInt(256));
-                board.get(x, y).setColor(color);
-                board.get(x, y).setNumber(++clickNum);
-                for (int i = 0; i < NUM_COLORED_FIELDS;) {
-                    Point point = points.remove(points.size() - 1);
-                    if (board.get(point).getColor() == null) {
-                        board.get(point).setColor(color);
-                        board.get(point).setNumber(clickNum);
-                        refresh(point.x, point.y);
-                        i++;
-                    }
-                }
-                refresh(x, y);
-                if (board.isOver()) {
-                    timer.stop();
-                    JOptionPane.showMessageDialog(boardPanel, "You have won in " + elapsedTime() + " ms.", "Congrats!",
-                            JOptionPane.PLAIN_MESSAGE);
-                }
-
-            }
-        }
-    }
-
-    public JLabel getTimeLabel() {
-        return timeLabel;
-    }
-
 }
